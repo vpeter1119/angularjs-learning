5790396489889647d9d9d7f5bd0351fd21d8d2ab
vpeter1119 <vpeter1119@gmail.com>
2021-07-19 18:10:56 +0200

    Try to set up papaparse

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,18 @@
 app.controller('MainController', ['$scope', function ($scope) {
     $scope.title = 'Szia Uram!';
+    $scope.result = [];
+    var scope = this.$scope;
+    var papaConfig = {
+        delimiter: ',',
+        download: true,
+        header: true,
+        complete: function (results) {
+            console.log(results.data);
+            //$scope.result = results.data;
+            scope.result = ["hello world"];
+        } 
+    }
+    $scope.parse = function() { 
+        Papa.parse('http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/CAN.csv', papaConfig);
+    }
 }]);
