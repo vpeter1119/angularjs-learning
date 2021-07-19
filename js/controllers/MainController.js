b1683ebc4a3560ef5fb8242e2d9dcbaee2300135
Peter Vertesi <vpeter1119@gmail.com>
2021-07-19 21:03:59 +0200

    Finalize csv parsing

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,15 +1,13 @@
 app.controller('MainController', ['$scope', function ($scope) {
     $scope.title = 'Szia Uram!';
-    $scope.result = [];
+    $scope.years = [];
     $scope.parse = function() {
         Papa.parse('http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/CAN.csv', {
             delimiter: ',',
             download: true,
             header: true,
-            complete: function (results) {
-                result = results.data;
-                console.log(result);
-                $scope.result = result;
+            complete: function (results) {  
+                $scope.years= results.data;
                 $scope.$apply();
             }
         });
